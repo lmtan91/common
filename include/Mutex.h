394bbdd4097541eecf729e40c47b3c0505d77e03
Le Minh Tan (RBVH/ENG25) <Tan.LeMinh@vn.bosch.com>
2017-09-27 15:53:38 +0700

    Add Mutex

--- include/Mutex.h
+++ include/Mutex.h
@@ -19,6 +19,7 @@
  * INCLUDE FILES
  *****************************************************************************/
 #include <pthread.h>
+#include <errno.h>
 
 /**============================================================================
  * Class description
@@ -33,9 +34,17 @@ public:
    /**=========================================================================
     * @brief Constructor
     *
-    * @param[in]
+    * @param[in]  recursive   Is mutex recursive
     *=========================================================================*/
-   Mutex();
+   Mutex( bool recursive );
+
+   /**=========================================================================
+    * @brief Constructor
+    *
+    * @param[in]  name        The name of mutex
+    * @param[in]  recursive   Is mutex recursive
+    *=========================================================================*/
+   Mutex( const char *name, bool recursive = false );
 
    /**=========================================================================
     * @brief Destructor
@@ -46,13 +55,116 @@ public:
    virtual ~Mutex();
 
    /**=========================================================================
-    * @brief Method description
+    * @brief Lock mutex until, blocking until lock is available.
     *
-    *
-    * @param[in]
-    * @param[out]
+    * @return int
     *=========================================================================*/
+   int Lock();
+
+   /**=========================================================================
+    * @brief Lock mutex without blocking.
+    *
+    * @return int
+    *=========================================================================*/
+   int TryLock();
+
+   /**=========================================================================
+    * @brief Unlock this mutex
+    *
+    * @return int
+    *=========================================================================*/
+   int Unlock();
+
+   /**=========================================================================
+    * @brief Block till lock is available.
+    *
+    * @return int
+    *=========================================================================*/
+   int TraceLock( const char *file, int line );
+
+   /**=========================================================================
+    * @brief Try to lock without blocking.
+    *
+    * @return int
+    *=========================================================================*/
+   int TraceTryLock( const char *file, int line );
+
+   /**=========================================================================
+    * @brief Unlock this mutex and print out trace info on error
+    *
+    * @return int
+    *=========================================================================*/
+   int TraceUnlock( const char *file, int line );
+
+   /**=========================================================================
+    * @brief Is this lock held by thois thread?
+    *
+    * @return bool
+    *=========================================================================*/
+   bool isLocked();
+
+   /**=========================================================================
+    * @brief What thread has locked this
+    *
+    * @return const char*
+    *=========================================================================*/
+   const char *getOwner();
+
+   /**=========================================================================
+    * @brief Global initialization of Mutex object
+    *
+    *=========================================================================*/
+   static void Init()
+   {
+      if ( !mInited ) {
+         mInited = true;
+         pthread_mutex_init( &mCriticalSection, NULL );
+      }
+   }
+
+   /**=========================================================================
+    * @brief Global destruction of Mutex object
+    *
+    *=========================================================================*/
+   static void Destroy()
+   {
+      pthread_mutex_destroy( &mCriticalSection );
+   }
+   /**=========================================================================
+    * @brief Grab global lock.
+    *
+    * Mutex::mCriticalSection is used in places scatterd around as a global lock
+    * It should NOT be held for longer than a couple of instructions
+    *=========================================================================*/
+   static void EnterCriticalSection()
+   {
+      pthread_mutex_lock( &mCriticalSection );
+   }
+
+   /**=========================================================================
+    * @brief Release global lock.
+    *
+    * Mutex::mCriticalSection is used in places scatterd around as a global lock
+    * It should NOT be held for longer than a couple of instructions
+    *=========================================================================*/
+   static void ExitCriticalSection()
+   {
+      pthread_mutex_unlock( &mCriticalSection );
+   }
 private:
+
+   /**=========================================================================
+    * @brief Create the internal lock that will be used. Pay attention to
+    * the mRecursive member to determine which attributes to set
+    *
+    *=========================================================================*/
+   void create_lock();
+
+   // The lock for this particular Mutex object
+   pthread_mutex_t mMutex;
+
+   // Who lock this lock
+   Thread *mLockedBy;
 
    // The name of this lock
    const char *mName;
@@ -81,6 +193,8 @@ private:
    int mLockLine;
    static pthread_mutex_t mCriticalSection;
    static bool mInited;
+
+
 };
 
 #endif /* ifndef INCLUDE_MUTEX_H_ */
--- src/Mutex.cpp
+++ src/Mutex.cpp
@@ -5,11 +5,11 @@
  * ALL RIGHTS RESERVED
  *
  * This software is the confidential and proprietary information of
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  *============================================================================*/
 
 /*
@@ -19,11 +19,157 @@
  *      Author: Tan.leMinh
  */
 
-#include "../include/Mutex.h"
+#include "Mutex.h"
 
-Mutex::Mutex() {
+Mutex::Mutex( bool recursive ) :
+      mLockedBy( NULL ), mName( NULL ), mRecursive( recursive ), mLockCount( 0 )
+{
+   create_lock();
 }
 
-Mutex::~Mutex() {
+Mutex::Mutex( const char *name, bool recursive ) :
+      mLockedBy( NULL ), mName( name ), mRecursive( recursive ), mLockCount( 0 )
+{
+   create_lock();
 }
 
+Mutex::~Mutex()
+{
+   int ret;
+
+   ret = pthread_mutex_destroy( &mMutex );
+   if ( ret ) {
+      printf( "pthread_mutex_destroy failed with %d\n", ret );
+   }
+}
+
+const char *Mutex::getOwner()
+{
+   if ( mLockedBy ) {
+      return mLockedBy->GetName();
+   } else {
+      return NULL;
+   }
+}
+
+bool Mutex::isLocked()
+{
+   return ( mLockedBy == Thread::GetCurrent() );
+}
+
+int Mutex::TraceUnlock(const char *file, int line)
+{
+   int ret;
+   Thread *lockedBy = mLockedBy;
+   int lockCount = mLockCount;
+   if ( mRecursive && --mLockCount <= 0 ) {
+      mLockedBy = NULL;
+      mLockCount = 0;
+   }
+   else {
+      mLockedBy = NULL;
+   }
+
+   ret = pthread_mutex_unlock( &mMutex );
+
+   // Check to see if operation not permmited
+   if ( EPERM == ret ) {
+      printf( "Unlock lock %s that isn't locked by current thread at %s:%d "
+            "(help by %s@%d:%d) error %d\n", mName, file, line,
+            lockedBy != NULL ? lockedBy->GetName() : "nobody", mLockFile,
+            mLockLine, ret );
+
+      mLockedBy = lockedBy;
+      mLockCount = lockCount;
+   }
+   else if ( ret != 0 ) {
+      printf( "Unlock lock %s failed with error %d at %s:%d", mName, ret, file,
+            line );
+   }
+   return ret;
+}
+
+int Mutex::TraceTryLock( const char *file, int line )
+{
+   int ret;
+
+   ret = pthread_mutex_trylock( &mMutex );
+   // If we locked successfully
+   if ( 0 == ret ) {
+      mLockedBy = Thread::GetCurrent();
+      mLockFile = file;
+      mLockLine = line;
+
+      if ( mRecursive && 0 == mLockCount ) {
+         ++mLockCount;
+      }
+   }
+
+   return ret;
+}
+
+int Mutex::TraceLock( const char *file, int line )
+{
+   int ret;
+
+   ret = pthread_mutex_lock( &mMutex );
+   if ( EDEADLK == ret ) {
+      printf( "Lock %s already taken at %s:%d\n", mName, mLockFile, mLockLine );
+   }
+   else if ( ret != 0 ) {
+      printf( "Lock %s failed with error %d at %s:%d\n", mName, mLockFile,
+            mLockLine );
+      return ret;
+   }
+
+   mLockedBy = Thread::GetCurrent();
+   mLockFile = file;
+   mLockLine = line;
+   if ( mRecursive && 0 == mLockCount ) {
+      ++mLockCount;
+   }
+   return ret;
+}
+
+int Mutex::Unlock()
+{
+   TraceLock( "unknown", 0 );
+}
+
+int Mutex::TryLock()
+{
+   TraceTryLock( "unknown", 0 );
+}
+
+int Mutex::Lock()
+{
+   TraceLock( "unknown", 0 );
+}
+
+void Mutex::create_lock()
+{
+   int ret;
+   int type;
+
+   pthread_mutexattr_t attr;
+   pthread_mutexattr_init( &attr );
+
+   if ( mRecursive ) {
+      type = PTHREAD_MUTEX_RECURSIVE_NP;
+   }
+   else {
+      type = PTHREAD_MUTEX_ERRORCHECK_NP;
+   }
+   ret = pthread_mutexattr_settype( &attr, type );
+
+   ret = pthread_mutex_init( &mMutex, &attr );
+   if ( ret ) {
+      printf( "pthread_mutex_init failed with %d\n", ret );
+   }
+
+   // destroy attribute of mutex
+   ret = pthread_mutexattr_destroy( &attr );
+   if ( ret ) {
+      printf( "pthread_mutexattr_destroy failed with %d\n", ret );
+   }
+}
