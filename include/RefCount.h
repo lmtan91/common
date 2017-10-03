49db62d6772de33ab949633ea4b5e37d27fd8955
Le Minh Tan (RBVH/ENG25) <Tan.LeMinh@vn.bosch.com>
2017-10-03 10:20:17 +0700

    RefCount - add SmartPtr

--- include/RefCount.h
+++ include/RefCount.h
@@ -18,11 +18,13 @@
 /*****************************************************************************
  * INCLUDE FILES
  *****************************************************************************/
-#include "Mutex.h"
+/* Standard */
+#include <stdio.h>
 
-enum ErrorCode {
-   kNoError, kNoInterface, kNoClass, kNoFactory, kLoadFailed
-};
+#include "Alias.h"
+#include "Mutex.h"
+#include "COM.h"
+
 
 /**============================================================================
  * Class description
@@ -110,4 +112,158 @@ private:
    mutable int mRefCount;
 };
 
+class SmartPtrHelper {
+public:
+   virtual COM::ErrorCode operator()( const COM::IID&, void** ) const
+   {
+      return COM::kNoError;
+   }
+protected:
+   virtual ~SmartPtrHelper()
+   {
+   }
+};
+
+template<class T>
+class SmartPtr {
+public:
+   SmartPtr() :
+            mObj( NULL )
+   {
+   }
+
+   SmartPtr( T *obj ) :
+            mObj( obj )
+   {
+      if ( mObj ) {
+         mObj->AddRef();
+      }
+   }
+
+   SmartPtr( const SmartPtr &ptr ) :
+            mObj( ptr.mObj )
+   {
+      if ( mObj ) {
+         mObj->AddRef();
+      }
+   }
+
+   SmartPtr( const SmartPtrHelper &helper ) :
+            mObj( NULL )
+   {
+      helper( COM_GET_IID( T ), Alias<void**, T**>::create( &mObj ) );
+   }
+
+   virtual ~SmartPtr()
+   {
+      if ( mObj ) {
+         mObj->Release();
+      }
+   }
+
+   SmartPtr &operator=( const SmartPtr &ptr )
+   {
+      // Use SmartPtr &operator=(T *obj) to handle the reference counting
+      *this = ptr.mObj;
+      return *this;
+   }
+
+   SmartPtr &operator=( const SmartPtrHelper &helper )
+   {
+#if 0
+      void *objPtr = reinterpret_cast<void*>( &mObj );
+      T *oldObj = reinterpret_cast<T*>( atomic_exchange_acq(
+               reinterpret_cast<int*>( objPtr ), NULL ) );
+
+      if ( oldObj != NULL )
+         oldObj->Release();
+#else
+      if ( mObj != NULL )
+      mObj->Release();
+      mObj = NULL;
+#endif
+      helper( COM_GET_IID( T ), Alias<void**, T**>::create( &mObj ) );
+      return *this;
+   }
+
+   SmartPtr &operator=( T *obj )
+   {
+      if ( obj != NULL ) {
+         obj->AddRef();
+      }
+#if 0
+      void *objPtr = reinterpret_cast<void*>( &mObj );
+      T *oldObj = reinterpret_cast<T*>( atomic_exchange_acq(
+               reinterpret_cast<int*>( objPtr ), (int) obj ) );
+
+      if ( oldObj != NULL )
+         oldObj->Release();
+#else
+      if ( mObj != NULL )
+      mObj->Release();
+      mObj = obj;
+#endif
+      return *this;
+   }
+
+   bool operator==( const T *obj ) const
+   {
+      printf( "SmartPtr::operator==( const T *obj ) Enter()\n" );
+      return ( mObj == obj );
+   }
+   bool operator==( const SmartPtr &ptr ) const
+   {
+      printf( "SmartPtr::operator==( const SmartPtr &ptr ) Enter()\n" );
+      return ( mObj == ptr.mObj );
+   }
+   bool operator!=( const T *obj ) const
+   {
+      printf( "SmartPtr::operator!=( const T *obj ) Enter()\n" );
+
+      return ( mObj != obj );
+   }
+   bool operator!=( const SmartPtr &ptr ) const
+   {
+      printf( "SmartPtr::operator!=( const SmartPtr &ptr ) Enter()\n" );
+
+      return ( mObj != ptr.mObj );
+   }
+
+   T* operator->()
+   {
+      printf( "SmartPtr::operator->() Enter()\n" );
+
+      return mObj;
+   }
+   const T* operator->() const
+   {
+      printf( "SmartPtr::operator->() const\n" );
+
+      return mObj;
+   }
+
+   operator T*()
+   {
+      printf( "SmartPtr::operator T*()\n" );
+
+      return mObj;
+   }
+
+   operator const T*() const
+   {
+      printf( "SmartPtr::operator const T*() const\n" );
+
+      return mObj;
+   }
+
+   const T* getObjectForDebug() const
+   {
+      printf( "SmartPtr::getObjectForDebug\n" );
+
+      return mObj;
+   }
+
+private:
+   T *mObj;
+};
 #endif /* ifndef INCLUDE_REFCOUNT_H_ */
