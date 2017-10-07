3c1d2de5e47eb777a70c51a89ef69b51d0171769
Tan.LeMinh <lmtan91@gmail.com>
2017-10-07 13:02:00 +0700

    conditionTest add test cases for condition

--- test/conditionTest.cpp
+++ test/conditionTest.cpp
@@ -25,12 +25,42 @@
 #include <iostream>
 
 using namespace std;
+Condition c;
+class TestClass {
+public:
+   TestClass();
+   virtual ~TestClass();
+
+   void testFunc();
+private:
+   Runnable<TestClass> *mThread;
+};
+
+TestClass::TestClass() {
+   mThread = new Runnable<TestClass>( "conditionTest", this,
+      &TestClass::testFunc );
+
+   mThread->Start();
+}
+
+TestClass::~TestClass() {
+   delete mThread;
+}
+
+void TestClass::testFunc() {
+   cout << "TestClass::testFunc()\n";
+   c.Broadcast();
+}
+
 int main( int argc, char* argv[] )
 {
-   Condition c;
+   TestClass *t = new TestClass;
+
    Mutex m;
    AutoLock l( m );
 
    cout << "hehe=" << c.Wait( m, 10000 ) << endl;
+
+   delete t;
    return 0;
 }
